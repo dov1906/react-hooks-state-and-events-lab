b7779f12d64163b091faf09c7a1ebc3e505315d2
Dov Azogui <dov.azogui@gmail.com>
2024-09-23 18:53:00 -0400

    created functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
+  // State variable to track if dark mode is enabled
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  
+  // Toggle between dark and light mode
+  function handleClick() {
+    setIsDarkMode((isDarkMode) => !isDarkMode);
+  }
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  // Use the state variable to determine the class name
+  const appClass = isDarkMode ? "App dark" : "App light";
+  
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleClick}>{appClass ? "Light Mode" : "Dark Mode"}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  // State to track the selected category for filtering
+  const [filterBy, setFilterBy] = useState("All");
+
+  // Handler to update the filterBy state when the user selects a different option
+  function handleFilterChange(event) {
+    setFilterBy(event.target.value);
+  }
+
+  // Filter the items based on the selected category
+  const itemsToDisplay = items.filter((item) => {
+    // Show all items if "All" is selected, otherwise filter by the selected category
+    if (filterBy === "All") {
+      return true;
+    } else {
+      return item.category === filterBy;
+    }
+  });
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
+        <select onChange={handleFilterChange} name="filter">
+          <option value="All">All</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
